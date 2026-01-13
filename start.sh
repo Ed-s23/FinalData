c4269ea5fdd2348cc03383cabe04dd197ef2260a
ED <laloangeles117@gmail.com>
2026-01-12 20:50:25 -0600

    ya no puedo mas

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Fallback por si PORT viene vacío
-PORT=${PORT:-8080}
-
 echo "PORT usado: $PORT"
 
+export DJANGO_SETTINGS_MODULE=FinalDataset.settings
+
 exec gunicorn FinalDataset.wsgi:application \
-  --bind 0.0.0.0:$PORT \
+  --bind 0.0.0.0:${PORT} \
   --workers 1 \
   --threads 2 \
   --timeout 120
